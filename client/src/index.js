19bf1ca070fdf4b383e519bc02d5a159fc110152
Barry <abdoulay@gmail.com>
2024-06-10 15:57:56 +0200

    fix: gérer l'erreur de limite de boucle de ResizeObserver
    Ajout d'un polyfill pour intercepter et supprimer l'erreur "ResizeObserver loop limit exceeded" dans le fichier index.js. Cela garantit que l'application ne plante pas à cause de cette erreur et améliore la stabilité globale.

--- client/src/index.js
+++ client/src/index.js
@@ -4,4 +4,18 @@ import './styles/index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<BrowserRouter> <App /> </BrowserRouter>, document.getElementById('root'));
+// Polyfill pour ResizeObserver error
+const resizeObserverLoopErrRe = /^ResizeObserver loop limit exceeded/;
+
+window.addEventListener('error', (event) => {
+  if (resizeObserverLoopErrRe.test(event.message)) {
+    event.stopImmediatePropagation();
+  }
+}, true);
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
